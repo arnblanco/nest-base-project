fb5c4d554aa013fe47708f7c7bdfe51047571806
Arnoldo Blanco <arnoldo.blanco@macropay.mx>
2024-05-16 10:38:21 -0600

    Configuracion de Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        url: `postgres://${ process.env.POSTGRES_USER }:${ process.env.POSTGRES_PASSWORD }@${ process.env.DB_HOST }:${ +process.env.DB_PORT }/${ process.env.POSTGRES_DB }`,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+    UsersModule,
+
+    AuthModule
+    
+  ],
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Aplication prefix: localhost:3000/api
+  app.setGlobalPrefix('api');
+  
+  //Secure input data
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      }
+    }),
+  );
+
+  //Documentation information config.
+  const config = new DocumentBuilder()
+  .setTitle('Nest Base Project')
+  .setDescription('API documentation for the base project.')
+  .setVersion('1.0')
+  .addTag('NBP')
+  .build();
+
+  //Documentation lounch
+  const document = SwaggerModule.createDocument( app, config );
+  SwaggerModule.setup( 'docs', app, document );
+
+  //Lounch app.
   await app.listen(3000);
 }
+
 bootstrap();
